70d0073157cc78d3a8795584617da1ced39b4292
xaota <1172619+xaota@users.noreply.github.com>
2019-12-03 18:43:17 +0500

    Роуты

--- DimmyOutRequestHandler.cs
+++ DimmyOutRequestHandler.cs
@@ -4,11 +4,16 @@ using Modulbank.Serverless;
 using Modulbank.Serverless.Contracts;
 using Modulbank.Serverless.Contracts.RequestHandlers;
 using Modulbank.Serverless.Contracts.RequestHandlers.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Modulbank.Serverless.Contracts.Attributes;
 
 namespace FirstService
 {
     public class DimmyOutRequestHandler : InternalOutRequestHandler<DimmyOut>
     {
+        [Authorize("system")]
+        [HttpGet]
+        [Route("dummy")]
         public override async Task<DimmyOut> Handle()
         {
             return new DimmyOut { Name = "Ринат" };
